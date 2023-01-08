eeecfc5a5e9faefa37ae5278d1528146e21cb1bd
johnson vj <121477725+samplejo@users.noreply.github.com>
2023-01-08 17:44:36 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,16 @@
+i=10
+while i<=10:
+    print(i)
+    i+=1
+print("loop Execution done")
+
+#break
+for i in range(10):
+    if i==7:
+        print("processing is enough.. pls  break")
+        continue
+    print(i)
+print("We are done with loop execution")
+
+
+
