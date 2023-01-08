c719fd40276310b11904990dbb7631909b945a83
johnson vj <121477725+samplejo@users.noreply.github.com>
2023-01-08 17:41:10 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,40 @@
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/2)
+print(a**2)
+print(a%3)
+#floor division
+print(5//2)
+print(-5//2)
+print("Hello"*2)
+print([1,2,3]*2)
+a=100
+print(a)
+print(a==100)#equality operator
+print(a>500)
+print(a>=100)
+print(a==1000)
+total=0
+print(total)
+total=total+100
+print(total)
+#short hand operators
+total+=100
+print(total)
+total*=100
+print(total)
+total/=100
+print(total)
+total%=100
+print(total)
+total-=100
+print(total)
+total**=100
+print(total)
+
+
+
+
