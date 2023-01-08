5ce9763ee7f4c5ecc1c2cc17ab1784e2839dbd71
johnson vj <121477725+samplejo@users.noreply.github.com>
2023-01-08 17:33:31 +0530

    Add files via upload

---
+++ copy.py
@@ -0,0 +1,18 @@
+#copy function
+s={1,2,3,4,5,6,7}
+s1=s.copy()
+print(s1)
+print(s)
+s3={1,2,3,4}
+v=s3.pop()
+print(v)
+print(s3)
+#remove function
+s4={2,3,4,5,7,8}
+s4.remove(8)
+print(s4)
+#discard function
+s4={2,3,4,5,7,8}
+s4.discard(100)
+print(s4)
+
