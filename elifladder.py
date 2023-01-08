a270181665e5cf9b2a5798080780dde848d07602
johnson vj <121477725+samplejo@users.noreply.github.com>
2023-01-08 17:36:42 +0530

    Add files via upload

---
+++ elifladder.py
@@ -0,0 +1,12 @@
+brand=input("Enter your favourite Brand:")
+if brand=="XY":
+    print("It is children brand")
+elif brand=="AB":
+    print("It is women brand")
+elif brand=="PQ":
+    print("It is men brand")
+else:
+    print("other Brands are not available")
+
+
+    
